e10903549c8daeaf17734a18be3316d425630f23
Tarcisio Ortega <tarcisiocaetano2009@gmail.com>
2021-07-09 12:04:31 -0300

    LoginPayload.cs

---
+++ NubankClient/Models/LoginPayload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tcortega.NubankClient.Models
+{
+    class LoginPayload
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public string public_key { get; set; }
+        public string public_key_crypto { get; set; }
+        public string model { get; set; }
+        public string device_id { get; set; }
+    }
+}
