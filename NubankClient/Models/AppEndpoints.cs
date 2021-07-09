34935a58cdcfa725aaf071763b42442ee0242617
Tarcisio Ortega <tarcisiocaetano2009@gmail.com>
2021-07-09 12:04:24 -0300

    AppEndPoints.cs

---
+++ NubankClient/Models/AppEndpoints.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tcortega.NubankClient.Models
+{
+    class AppEndpoints
+    {
+        public string scopes { get; set; }
+        public string creation { get; set; }
+        public string change_password { get; set; }
+        public string smokejumper { get; set; }
+        public string block { get; set; }
+        public string lift { get; set; }
+        public string shard_mapping_id { get; set; }
+        public string force_reset_password { get; set; }
+        public string revoke_token { get; set; }
+        public string userinfo { get; set; }
+        public string reset_password { get; set; }
+        public string unblock { get; set; }
+        public string shard_mapping_cnpj { get; set; }
+        public string shard_mapping_cpf { get; set; }
+        public string register_prospect { get; set; }
+        public string engage { get; set; }
+        public string creation_with_credentials { get; set; }
+        public string magnitude { get; set; }
+        public string revoke_all { get; set; }
+        public string user_update_credential { get; set; }
+        public string user_hypermedia { get; set; }
+        public string gen_certificate { get; set; }
+        public string email_verify { get; set; }
+        public string token { get; set; }
+        public string account_recovery { get; set; }
+        public string start_screen_v2 { get; set; }
+        public string scopes_remove { get; set; }
+        public string approved_products { get; set; }
+        public string admin_revoke_all { get; set; }
+        public string scopes_add { get; set; }
+        public string registration { get; set; }
+        public string global_services { get; set; }
+        public string start_screen { get; set; }
+        public string user_change_password { get; set; }
+        public string account_recovery_token { get; set; }
+        public string user_status { get; set; }
+        public string engage_and_create_credentials { get; set; }
+    }
+}
