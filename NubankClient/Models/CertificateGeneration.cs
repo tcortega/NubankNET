67e8d3e7f9bb6510aa2a464e6fa9d1e6f580eeea
Tarcisio Ortega <tarcisiocaetano2009@gmail.com>
2021-07-10 10:21:30 -0300

    CertificateGeneration Model

---
+++ NubankClient/Models/CertificateGeneration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tcortega.NubankClient.Models
+{
+    class CertificateGeneration
+    {
+        public string certificate { get; set; }
+        public string certificate_crypto { get; set; }
+    }
+}
